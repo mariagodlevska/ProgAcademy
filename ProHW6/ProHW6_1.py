c4cf27e80ce7d5da011b80bde79f5cc2e516923d
Maria <idealniy.ringo@gmail.com>
2023-01-12 22:39:23 +0200

    ProHW6 Added problem 3 (3 different approaches. 1: modified HW6; 2: Sieve of Eratosthenes, adding; 3: Sieve of Eratosthenes, removing). Method 3 is the most time efficient

--- ProHW6/ProHW6_1.py
+++ ProHW6/ProHW6_1.py
@@ -2,6 +2,7 @@
 # Генератор повинен зупинити свою роботу або після досягнення зазначеного елементу,
 # або при передачі команди на завершення.
 
+
 def geometric_prog(d: int, end_fun, element=1):
     for i in range(end_fun):
         yield element
@@ -28,5 +29,71 @@ for i in new_range(10, 2):
 
 # 3. Напишіть функцію-генератор, яка повертатиме прості числа.
 # Верхня межа діапазону повинна бути задана параметром цієї функції.
+import timeit
+
+
+def prime_numbers(finish=100, start=2):
+    num_list = [2]
+
+    for start in range(start, finish):
+        for i in num_list:
+            if not start % i:
+                break
+        else:
+            yield start
+            num_list.append(start)
+
+
+h = []
+for n in prime_numbers(100):
+    h.append(n)
+print(h)
+
+
+def prime_num1(finish=100, start=2):
+    yield 1
+    not_num_list = []
+    # num_list = list(range(1, finish))
+    while start < finish:
+        if start in not_num_list:
+            start += 1
+
+        else:
+            for i in range(start*start, finish+1, start):
+                not_num_list.append(i)
+            yield start
+            start += 1
+
+
+b = []
+for n in prime_num1(100):
+    b.append(n)
+print(b)
+
+
+def prime_num2(finish=100, start=2):
+    yield 1
+    not_num_list = []
+    num_list = list(range(1, finish))
+    while start < finish:
+        if start not in num_list:
+            start += 1
+
+        else:
+            for i in range(start * start, finish + 1, start):
+                if i in num_list:
+                    num_list.remove(i)
+            yield start
+            start += 1
+
+
+d = []
+for n in prime_num2(100):
+    d.append(n)
+print(d)
+
+
 # 4. Напишіть генераторний вираз для заповнення списку.
 # Список повинен бути заповнений кубами чисел від 2 до вказаної вами величини.
+
+
